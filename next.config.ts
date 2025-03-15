6bd5ad3672dd1e8a7c161ec93d489b5f31e034f3
Naol Denko <naoldenko@outlook.com>
2025-03-14 21:03:04 -0400

    fix connection to domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/contentility-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/contentility-website/' : '',
   images: {
     unoptimized: true,
   },
