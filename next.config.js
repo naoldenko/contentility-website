081cf03fd1f9f2a47645fa4d1b6f2989664fabce
Naol Denko <naoldenko@outlook.com>
2025-03-26 18:51:13 -0400

    fix: update deployment configuration for custom domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/contentility-website' : '',
   images: {
     unoptimized: true,
   },
